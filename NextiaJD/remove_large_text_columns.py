e2f089ed181afb29aeaf02965b8b660ba5acf217
azimafroozeh <afroozeh3@gmail.com>
2025-05-31 19:50:09 +0200

    remove over 500 char columns

--- NextiaJD/remove_large_text_columns.py
+++ NextiaJD/remove_large_text_columns.py
@@ -28,7 +28,7 @@ from typing import List, Set, Tuple
 N_ROWS = 64 * 1024
 # maximum allowed length for any single cell; columns that violate this
 # will be entirely removed from the sample
-MAX_CELL_LEN = 1000
+MAX_CELL_LEN = 500
 
 
 def load_delimiters(metadata_path: Path) -> dict:
